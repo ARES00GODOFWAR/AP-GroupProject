5622028243b7de071ba0b54159bf1f1781a6c4ff
Stephen Leslie <stephenkleslie@students.utech.edu.jm>
2022-03-19 19:43:06 -0500

    Employee class added

---
+++ src/main/java/org/southstar/model/CustomerServiceReps.java
@@ -0,0 +1,7 @@
+package org.southstar.model;
+
+
+public class CustomerServiceReps extends Employees {
+
+
+}
---
+++ src/main/java/org/southstar/model/Employees.java
@@ -0,0 +1,22 @@
+package org.southstar.model;
+
+public class Employees {
+    private String staffID;
+    private String password;
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
